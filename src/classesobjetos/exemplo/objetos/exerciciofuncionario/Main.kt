9e6490d2aa0fb7c09076e168f421b72d9822d98b
jeehmilena <jessicamilenacorrea@gmail.com>
2022-04-26 12:10:54 -0300

    resolução do exercício 1 sobre funcionario

---
+++ src/classesobjetos/exemplo/objetos/exerciciofuncionario/Main.kt
@@ -0,0 +1,41 @@
+package classesobjetos.exemplo.objetos.exerciciofuncionario
+
+fun main() {
+    val novoFuncionario: Funcionario = Funcionario()
+
+    novoFuncionario.nome = "Luis"
+    novoFuncionario.sobrenome = "Santos"
+    novoFuncionario.horasTrabalhadas = 10.0
+    novoFuncionario.valorPorHora = 25.50
+
+    novoFuncionario.nomeCompleto()
+    novoFuncionario.calcularSalario()
+    novoFuncionario.incrementarHoras(8.0)
+    novoFuncionario.calcularSalario()
+
+    val listaFuncionarios = ArrayList<Funcionario>()
+
+    val funcionarioCarol: Funcionario = Funcionario()
+    funcionarioCarol.nome = "Carol"
+    funcionarioCarol.sobrenome = "Fonseca"
+    listaFuncionarios.add(funcionarioCarol)
+
+    val funcionarioJessica: Funcionario = Funcionario()
+    funcionarioJessica.nome = "Jessica"
+    funcionarioJessica.sobrenome = "Santos"
+    listaFuncionarios.add(funcionarioJessica)
+
+    val funcionarioThayana: Funcionario = Funcionario()
+    funcionarioThayana.nome = "Thayana"
+    funcionarioThayana.sobrenome = "Gayva"
+    listaFuncionarios.add(funcionarioThayana)
+
+    listaFuncionarios.forEach { funcionario ->
+        funcionario.valorPorHora = 50.0
+        funcionario.horasTrabalhadas = 40.0
+        funcionario.nomeCompleto()
+        funcionario.calcularSalario()
+    }
+
+
+}
